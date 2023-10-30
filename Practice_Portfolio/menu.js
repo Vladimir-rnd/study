f12a81e7e092374bc510364ff1bb6175ea70cfd9
Vladimir-rnd <50573566+Vladimir-rnd@users.noreply.github.com>
2023-10-31 00:34:36 +0300

    Delete Practice_Portfolio directory
    Потому что эта папка переехала в отдельный репозиторий

--- Practice_Portfolio/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let burger = document.getElementById('burger');
-
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    document.getElementById('menu').classList.toggle('active');
-  });
-});
-
